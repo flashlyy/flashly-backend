27ce17356d2c0a6019c5e938c68e5a43c56da907
Vishal <technocratvenom@gmail.com>
2025-11-22 23:16:12 +0530

    connected to mongodb

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+const PORT = process.env.PORT || 8000;
+
+
+connectDB()
+  .then(() => {
+      app.listen(PORT, () => {
+          console.log(`Server running on port ${PORT}`);
+      });
+  })
+  .catch((err) => {
+      console.log("MongoDB connect error in index.js", err);
+  });
