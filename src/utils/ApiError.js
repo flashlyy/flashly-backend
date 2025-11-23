43390aff4bf87f1421d5a472b666c5f3387da072
Vishal <technocratvenom@gmail.com>
2025-11-23 12:04:33 +0530

    api error.js changes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,8 +1,26 @@
 class ApiError extends Error{
     constructor(
         statusCode,
-        message="something went wrong"
+        message="something went wrong",
+        errors = [],
+        statck =""
     )
-    {}
+    {
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message 
+        this.success = false;
+        this.errors = errors 
+
+        if(statck){
+            this.statck = this.statck
+        }else {
+
+            Error.captureStackTrace(this,this.constructor)
+        }
+        
+    }
 
 }
+export {ApiError}  
